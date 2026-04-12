7d50ceac7a7bc11da8453237ca54cf70fe77d0ec
Ciyanci <bryanyeoh2007@gmail.com>
2026-04-12 17:26:12 +0800

    player healing and damage testing

--- Bryan_unityFiles/Assets/scripts/GameManager.cs
+++ Bryan_unityFiles/Assets/scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 
     public HealthPoints _playerHealth = new HealthPoints(100, 100);
 
-    void Awoken()
+    void Awake()
     {
         if (gameManager != null && gameManager != this)
         {
---
+++ Bryan_unityFiles/Assets/scripts/PlayerBehaviour.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerTakeDmg(20);
+            Debug.Log(GameManager.gameManager._playerHealth.Health);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            PlayerHeal(10);
+            Debug.Log(GameManager.gameManager._playerHealth.Health);
+        }
+    }
+    private void PlayerTakeDmg(int dmg)
+    {
+        GameManager.gameManager._playerHealth.DmgPoint(dmg);
+    }
+    private void PlayerHeal(int healed)
+    {
+        GameManager.gameManager._playerHealth.healing(healed);
+    }
+}
