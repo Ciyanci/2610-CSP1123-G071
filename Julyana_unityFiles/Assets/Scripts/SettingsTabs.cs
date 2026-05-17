524b960d01144d3e38a1a0d3bcc7f2da7237a6b3
Ciyanci <bryanyeoh2007@gmail.com>
2026-05-17 18:10:38 +0800

    Update SettingsTabs.cs

--- Julyana_unityFiles/Assets/Scripts/SettingsTabs.cs
+++ Julyana_unityFiles/Assets/Scripts/SettingsTabs.cs
@@ -5,6 +5,7 @@ public class SettingsTabs : MonoBehaviour
     [SerializeField] private GameObject audioPanel;
     [SerializeField] private GameObject videoPanel;
     [SerializeField] private GameObject gameplayPanel;
+    [SerializeField] private GameObject controlsPanel;
 
     private void Start()
     {
@@ -16,17 +17,27 @@ public class SettingsTabs : MonoBehaviour
         audioPanel.SetActive(true);
         videoPanel.SetActive(false);
         gameplayPanel.SetActive(false);
+        controlsPanel.SetActive(false);
     }
     public void OpenVideo()
     {
         audioPanel.SetActive(false);
         videoPanel.SetActive(true);
         gameplayPanel.SetActive(false);
+        controlsPanel.SetActive(false);
     }
     public void OpenGameplay()
     {
         audioPanel.SetActive(false);
         videoPanel.SetActive(false);
         gameplayPanel.SetActive(true);
+        controlsPanel.SetActive(false);
+    }
+    public void OpenControls()
+    {
+        audioPanel.SetActive(false);
+        videoPanel.SetActive(false);
+        gameplayPanel.SetActive(false);
+        controlsPanel.SetActive(true);
     }
 }
