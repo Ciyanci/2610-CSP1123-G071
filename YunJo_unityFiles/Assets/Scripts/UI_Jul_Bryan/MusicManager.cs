8618c4a4b501a8ebbd72a32a90f7e0fb7c8dcef5
Ciyanci <bryanyeoh2007@gmail.com>
2026-06-28 19:06:57 +0800

    fixed bgm so that it wouldnt play in other scenes like the battle scene

--- YunJo_unityFiles/Assets/Scripts/UI_Jul_Bryan/MusicManager.cs
+++ YunJo_unityFiles/Assets/Scripts/UI_Jul_Bryan/MusicManager.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     private static MusicManager instance;
+
+    [Header("References")]
+    public AudioSource bgm;
+
+    [Header("StopBgm In These Scenes")]
+    public int[] silentSceneIndexes;
 
     private void Awake()
     {
@@ -13,5 +20,29 @@ public class MusicManager : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+     void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        foreach (var index in silentSceneIndexes)
+        {
+            if (scene.buildIndex == index)
+            {
+                bgm.Stop();
+                return;
+            }
+        }
+        if (!bgm.isPlaying)
+        bgm.Play();
     }
 }
