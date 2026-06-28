64f2df6fa57e9abf6446e72bd2523c59ff0781ce
shuuwiichi <engsyuu@gmail.com>
2026-06-28 22:05:07 +0800

    error fixes lol

--- YunJo_unityFiles/Assets/Scripts/StageRewards/RewardCardEntryUI.cs
+++ YunJo_unityFiles/Assets/Scripts/StageRewards/RewardCardEntryUI.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
-public class RewardCardEntryUI
+public class RewardCardEntryUI : MonoBehaviour
 {
-    
+    public Image           artwork;
+    public TextMeshProUGUI cardNameText;
+    public TextMeshProUGUI costText;
+    public Button          selectButton;
+
+    CardData boundCard;
+    Action<CardData> onSelected;
+
+    public void Setup(CardData card, Action<CardData> callback)
+    {
+        boundCard  = card;
+        onSelected = callback;
+
+        if (artwork      != null && card.Artwork != null)
+            artwork.sprite = card.Artwork;
+        if (cardNameText != null) cardNameText.text = card.Name;
+        if (costText     != null) costText.text     = card.Cost.ToString();
+
+        selectButton?.onClick.RemoveAllListeners();
+        selectButton?.onClick.AddListener(() => onSelected?.Invoke(boundCard));
+    }
 }
